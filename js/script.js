f1d7dd3481e5848f4bb827a80a647ecb230c9877
Alfattah <alfatahatalarais12@gmail.com>
2025-05-24 19:24:33 +0700

    Initial commit of project files

--- js/script.js
+++ js/script.js
@@ -6,11 +6,28 @@ document.querySelector("#hamburger-menu").onclick = (e) => {
   e.preventDefault();
 };
 
-// Klik diluar sidebar untuk menghilangkan nav
+// Toggle class active untuk search form
+const searchForm = document.querySelector(".search-form");
+const searchBox = document.querySelector("#search-box");
+
+document.querySelector("#search-btn").onclick = (e) => {
+  searchForm.classList.toggle("active");
+  searchBox.focus();
+  e.preventDefault();
+};
+
+// Klik diluar elemen
 const hamburger = document.querySelector("#hamburger-menu");
+const searchbtn = document.querySelector("#search-btn");
 
 document.addEventListener("click", function (e) {
   if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
     navbarNav.classList.remove("active");
+  }
+});
+
+document.addEventListener("click", function (e) {
+  if (!searchbtn.contains(e.target) && !searchForm.contains(e.target)) {
+    searchForm.classList.remove("active");
   }
 });
