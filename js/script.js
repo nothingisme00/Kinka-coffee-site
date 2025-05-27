c7d8754dbf78685493b3a4f9527f957f07d7380a
Alfattah <alfatahatalarais12@gmail.com>
2025-05-27 13:17:24 +0700

    Add shopping cart

--- js/script.js
+++ js/script.js
@@ -1,8 +1,16 @@
-// Toggle class active
+// Toggle class active hamburger menu
 const navbarNav = document.querySelector(".navbar-nav");
-// Ketika hamburger menu di klik
+
 document.querySelector("#hamburger-menu").onclick = (e) => {
   navbarNav.classList.toggle("active");
+  e.preventDefault();
+};
+
+// Toggle class active untuk shopping cart
+const shoppingcart = document.querySelector(".shopping-cart");
+
+document.querySelector("#shopping-cart-btn").onclick = (e) => {
+  shoppingcart.classList.toggle("active");
   e.preventDefault();
 };
 
@@ -19,6 +27,7 @@ document.querySelector("#search-btn").onclick = (e) => {
 // Klik diluar elemen
 const hamburger = document.querySelector("#hamburger-menu");
 const searchbtn = document.querySelector("#search-btn");
+const shoppingcartbtn = document.querySelector("#shopping-cart-btn");
 
 document.addEventListener("click", function (e) {
   if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
@@ -29,5 +38,11 @@ document.addEventListener("click", function (e) {
 document.addEventListener("click", function (e) {
   if (!searchbtn.contains(e.target) && !searchForm.contains(e.target)) {
     searchForm.classList.remove("active");
+  }
+});
+
+document.addEventListener("click", function (e) {
+  if (!shoppingcartbtn.contains(e.target) && !shoppingcart.contains(e.target)) {
+    shoppingcart.classList.remove("active");
   }
 });
